72a5b8d3d200a996da913089ad39ff76df8c06fe
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-03-19 22:37:39 +0300

    Dicee

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1 = UIImage(imageLiteralResourceName: "DiceTwo")
+        diceImageView2 = UIImage(imageLiteralResourceName: "DiceSix")
+        
     }
 
 
