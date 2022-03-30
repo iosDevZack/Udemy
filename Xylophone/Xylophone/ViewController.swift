40fb038a388479e1446c363b6cf187999b5b0c10
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-03-30 22:48:32 +0300

    Xylophone

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -12,21 +12,34 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        
+        playSound(soundName: sender.currentTitle!)
+        
+        
+        // Уменьшает непрозрачность отправителя (нажатой кнопки) до половины.
+        sender.alpha = 0.5
+        
+        // Код должен выполняться с задержкой в 0,2 секунды.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            
+        // Увеличьте непрозрачность отправителя до полной непрозрачности.
+            sender.alpha = 1.0
+        }
     }
     
     //Функция воспроизведения музыки
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
+        
     }
 }
 
