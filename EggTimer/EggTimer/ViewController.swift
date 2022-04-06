8aac3760a7f6b298efbe9f0679a17e77ebf7ff77
iosDevZack <86024420+iosDevZack@users.noreply.github.com>
2022-04-06 18:24:58 +0300

    EggTimer

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -7,27 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     
     var totalTime = 0
     var secondPassed = 0
     
     var timer = Timer()
+    
+    var player = AVAudioPlayer()
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-
         
         timer.invalidate()
-        
         let hardness = sender.currentTitle // Soft Medium Hard
-        
         totalTime = eggTimes[hardness!]!
+        
+        progressBar.progress = 0.0
+        secondPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
@@ -36,16 +40,18 @@ class ViewController: UIViewController {
     
     @objc func updateTimer() {
         if secondPassed < totalTime {
-            
-            let percentageProgress = secondPassed / totalTime
-            progressBar.progress = Float(percentageProgress)
-            
             secondPassed += 1
+            progressBar.progress = Float(secondPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
+            
+            guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+            player = try! AVAudioPlayer(contentsOf: url)
+            player.play()
         }
     }
+
 
 }
 
